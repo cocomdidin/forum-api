be44eea2725242052e308d24d3d55933c4126f71
cocom <udn.chomr@gmail.com>
2023-06-17 20:09:49 +0700

    migration create table threads

---
+++ migrations/1687003386125_create-table-threads.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      unique: true,
+    },
+    created_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('threads');
+};
