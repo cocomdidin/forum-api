1fcc5ea9955378fe45e13a919890b5aacbf6b526
cocom <udn.chomr@gmail.com>
2023-06-20 19:36:02 +0700

    make migration create table comments

---
+++ migrations/1687263022454_create-table-comments.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    threadId: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'threads',
+      onDelete: 'CASCADE',
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    commentId: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+      references: 'comments',
+      onDelete: 'CASCADE',
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users',
+    },
+    date: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('comments');
+};
